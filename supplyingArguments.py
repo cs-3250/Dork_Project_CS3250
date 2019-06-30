a90ba0114ef541a8db224d2de647baf22e1f280f
unknown <mhurt@msudenver.edu>
2019-06-30 10:44:11 -0600

    init

---
+++ supplyingArguments.py
@@ -0,0 +1,14 @@
+
+'''When defining a custom function in Python programming you may, optionally, 
+specify an "argument" name between the functions.  The function can now use 
+that passed in value during its execution by referencing it via the argument 
+name.  For example, defining a function to accept an argument to print out, 
+like this:'''
+def echo(user):
+    print('User:', user)
+'''A call to this function must specify a value to be passed to the argument 
+within its parentheses so it can be printed out:'''
+echo('Mike')
+
+"""
+"""
