45d766681597766f2b52d291724542915420c034
unknown <mhurt@msudenver.edu>
2019-06-30 10:34:32 -0600

    Spelling corrections.

--- understandingScope.py
+++ understandingScope.py
@@ -4,7 +4,7 @@ a number of custom functions that can be called as requried when the program run
 
 A custom function is created using the def (definition) keyword follwed by a 
 name of your choice and () parentheses.  The programmer can choose any name 
-for a cuntion except the Python keywords and the name of an existing built-in 
+for a function except the Python keywords and the name of an existing built-in 
 function.  This line must end with a colon : character, then the statements to 
 be executed whenever the function gets called must appear on lines below and 
 be indented. Syntax of a function definition, therefore, looks like this:
@@ -13,15 +13,15 @@ def function-name():
         statements-to-be-executed
         statements-to-be-executed
         
-Once the function statements have been exxecuted, program flow resumes at the 
-point directly folling the function call.  This modularity is very useful in 
+Once the function statements have been executed, program flow resumes at the 
+point directly following the function call.  This modularity is very useful in 
 Python programming to isolate set routines so they can be called upon repeatedly.
 
 To create custom functions it is necessary to understand the accessibility 
 ("Scope") of variables in a program:
 
     -Variables created inside functions cannot be referenced by statements 
-    inside functinos - they have "global" scope.
+    inside functions - they have "global" scope.
     -Variables created inside functions cannot be referenced from outside the 
     function in which they have been created - these have "local" scope.
     
@@ -34,11 +34,12 @@ To create custom functions it is necessary to understand the accessibility
     from anywher in the program.  Wher a global variable and a local variable 
     have the same name, the function will use the local version.
     
-    
--function statements must be indented from the definition line by the same 
+-Function statements must be indented from the definition line by the same 
 amount so the Python interpreter can recognize the block.
+
 -Avoid using global variables in order to prevent accidental conflict - use 
 only local variables where possible.
+
 -Variables that are not global but appear in some outer scope can be addressed 
 using the nonlocal keyword.    
 """
